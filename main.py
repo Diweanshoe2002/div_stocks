7b0980670bd9a7c31cbbf0d4ae036234d55ee575
Diweanshoe2002 <89806004+Diweanshoe2002@users.noreply.github.com>
2023-12-09 19:12:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -55,8 +55,8 @@ if selected == "HOMEPAGE":
     with open("access_token", 'r') as r:
         access_token = r.read()
     client_id = '0X7ILODH7P-100'
-    newtoken = f"{client_id}:{access_token}"
-    fyers = fyersModel.FyersModel(0X7ILODH7P-100, token=access_token, log_path=os.getcwd())
+    #newtoken = f"{client_id}:{access_token}"
+    fyers = fyersModel.FyersModel('0X7ILODH7P-100', token=access_token, log_path=os.getcwd())
     data = {"symbols": "NSE:NIFTY50-INDEX,NSE:NIFTYBANK-INDEX"}
     response = fyers.quotes(data=data)
     t = response['d'][0]
