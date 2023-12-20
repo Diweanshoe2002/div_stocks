cba6c06a13c3d95f320b009dc2ffcbfcdf4c339f
Diweanshoe2002 <89806004+Diweanshoe2002@users.noreply.github.com>
2023-12-20 21:15:03 +0530

    Update testt2.py

--- testt2.py
+++ testt2.py
@@ -1,26 +1,14 @@
+import streamlit as st
 import requests
-import streamlit
-import pandas as pd
-header = {
-    "Connection": "keep-alive",
-    "Cache-Control": "max-age=0",
-    "DNT": "1",
-    "Upgrade-Insecure-Requests": "1",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                  "Chrome/111.0.0.0 Safari/537.36",
-    "Sec-Fetch-User": "?1", "Accept": "*/*", "Sec-Fetch-Site": "none", "Sec-Fetch-Mode": "navigate",
-    "Accept-Encoding": "gzip, deflate, br", "Accept-Language": "en-US,en;q=0.9,hi;q=0.8"
-    }
 
-url="https://www.nseindia.com/api/historical/bulk-deals?from=18-12-2023&to=19-12-2023&csv=true"
+# FastAPI endpoint URL
+fastapi_url = "http://127.0.0.1:8000/get_bulk_data"
 
-def nse_urlfetch(url):
-    r_session = requests.session()
-    nse_live = r_session.get("http://nseindia.com", headers=header)
-    return r_session.get(url, headers=header)
+# Make a request to the FastAPI endpoint
+response = requests.get(fastapi_url)
 
-data_text = nse_urlfetch(url).text
-#f= open('BLUK.csv', 'w')
-#f.write(data_text)
-#f.close()
-streamlit.write(data_text)
+# Check if the request was successful (status code 200)
+if response.status_code == 200:
+    # Parse the JSON response from FastAPI
+    data = response.json()
+    st.json(data)
