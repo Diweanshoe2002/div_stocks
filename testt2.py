5084f0615d13141762b6dedd7b0fdf2cdf1e1534
Diweanshoe2002 <89806004+Diweanshoe2002@users.noreply.github.com>
2023-12-23 17:23:23 +0530

    Update testt2.py

--- testt2.py
+++ testt2.py
@@ -1,23 +1,6 @@
-import sqlite3
-import pandas as pd
+import redis
 import streamlit as st
-conn = sqlite3.connect('example.db')
+r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
-# Create a cursor object
-cursor = conn.cursor()
-
-# Execute a SQL query
-cursor.execute('SELECT * FROM fii_table')
-
-# Fetch all the results
-results = cursor.fetchall()
-
-# Close the cursor and the connection
-cursor.close()
-conn.close()
-
-# Create a pandas DataFrame from the results
-df = pd.DataFrame(results)
-
-# Print the DataFrame
-st.dataframe(df)
+C=r.get("INDEXDATA")
+st.json(C)
