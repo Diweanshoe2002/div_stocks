34a08d8f0f211914a956b1cb8e36d768fcc7e6cc
Diweanshoe2002 <89806004+Diweanshoe2002@users.noreply.github.com>
2023-12-24 16:48:56 +0530

    Update testt2.py

--- testt2.py
+++ testt2.py
@@ -4,8 +4,12 @@ import streamlit as st
 gc = gspread.service_account(filename='service_account.json')
 sheet_id = "1T79XwzC8sG7pMHaNXYug9BJ9uwseBtLbrLM0G4seBAc"
 sheet_name = "Sheet1"
+sheet_name1="Sheet2"
 link = "https://docs.google.com/spreadsheets/d/{}/gviz/tq?tqx=out:csv&sheet={}".format(sheet_id, sheet_name)
 sh = gc.open_by_url(link)
 ws = sh.worksheet(sheet_name)
+ws1 = sh.worksheet(sheet_name1)
 df = pd.DataFrame(ws.get_all_records())
 st.dataframe(df)
+df1 = pd.Dataframe(ws1.get_all_records( ))
+st.dataframe(df1)
