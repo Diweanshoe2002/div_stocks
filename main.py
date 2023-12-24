6ab8217889a61d2dfb48e482e0290dec67a82824
Diweanshoe2002 <89806004+Diweanshoe2002@users.noreply.github.com>
2023-12-24 17:19:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from streamlit_lottie import st_lottie
 from streamlit_option_menu import option_menu
 import streamlit as st
 from yfinance import Ticker
+import gspread
 from nsedt import equity as eq
 import pandas as pd
 import pandas_ta as ta
@@ -128,8 +129,15 @@ if selected == "HOMEPAGE":
     col66,col77=st.columns(2)
     with col66:
         st.subheader("FII/DII DATA")
-        response12=nse_headers_session("https://www.nseindia.com/api/fiidiiTradeReact")
-        fiidii=pd.json_normalize(response12)
+        gc = gspread.service_account(filename='service_account.json')
+        sheet_id = "1T79XwzC8sG7pMHaNXYug9BJ9uwseBtLbrLM0G4seBAc"
+        sheet_name = "Sheet4"
+        link = "https://docs.google.com/spreadsheets/d/{}/gviz/tq?tqx=out:csv&sheet={}".format(sheet_id, sheet_name)
+        sh = gc.open_by_url(link)
+        ws = sh.worksheet(sheet_name)
+        fiidii = pd.DataFrame(ws.get_all_records())
+        #response12=nse_headers_session("https://www.nseindia.com/api/fiidiiTradeReact")
+        #fiidii=pd.json_normalize(response12)
         st.write(" ")
         st.write(" ")
         st.write(" ")
