cc26413ea71c6f3a30272a74da7f24db08194abf
Diweanshoe2002 <89806004+Diweanshoe2002@users.noreply.github.com>
2023-12-24 16:28:09 +0530

    Update testt2.py

--- testt2.py
+++ testt2.py
@@ -1,6 +1,11 @@
-import redis
+import gspread
+import pandas as pd
 import streamlit as st
-r = redis.Redis(host='localhost', port=6379, decode_responses=True)
-
-C=r.get("INDEXDATA")
-st.json(C)
+gc = gspread.service_account(filename='service_account.json')
+sheet_id = "1T79XwzC8sG7pMHaNXYug9BJ9uwseBtLbrLM0G4seBAc"
+sheet_name = "Sheet1"
+link = "https://docs.google.com/spreadsheets/d/{}/gviz/tq?tqx=out:csv&sheet={}".format(sheet_id, sheet_name)
+sh = gc.open_by_url(link)
+ws = sh.worksheet(sheet_name)
+df = pd.DataFrame(ws.get_all_records())
+st.dataframe(df)
