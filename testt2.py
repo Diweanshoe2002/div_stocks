a1de0d5504725824abe4af090771d82916b08294
Diweanshoe2002 <89806004+Diweanshoe2002@users.noreply.github.com>
2023-12-31 17:10:55 +0530

    Update testt2.py

--- testt2.py
+++ testt2.py
@@ -1,12 +1,20 @@
-import gspread
-import pandas as pd
 import streamlit as st
-sheet_id = "1T79XwzC8sG7pMHaNXYug9BJ9uwseBtLbrLM0G4seBAc"
-sheet_name = "Sheet1"
-sheet_name1="Sheet2"
-link = "https://docs.google.com/spreadsheets/d/{}/gviz/tq?tqx=out:csv&sheet={}".format(sheet_id, sheet_name)
-df = pd.read_csv(link)
+import pandas as pd
+import requests
+
+def nse_headers_session(url):
+    baseurl = "https://www.nseindia.com/"
+    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, '
+                             'like Gecko) '
+                             'Chrome/80.0.3987.149 Safari/537.36',
+               'accept-language': 'en,gu;q=0.9,hi;q=0.8', 'accept-encoding': 'gzip, deflate, br'}
+    session = requests.Session()
+    request = session.get(baseurl, headers=headers)
+    cookies = dict(request.cookies)
+    response = session.get(url, headers=headers, cookies=cookies)
+    raw = (response.json())
+    return raw
+
+j=nse_headers_session("https://www.nseindia.com/api/corporates-pit?index=equities&from_date=01-01-2022&to_date=30-12-2023&symbol=ETHOSLTD")
+df=pd.DataFrame(j['data'])
 st.dataframe(df)
-link1 = "https://docs.google.com/spreadsheets/d/{}/gviz/tq?tqx=out:csv&sheet={}".format(sheet_id, sheet_name1)
-df1 = pd.DataFrame(link1)
-st.dataframe(df1)
