a7341d4fa07a60e434d58462ae20985b3f91792b
Diweanshoe2002 <89806004+Diweanshoe2002@users.noreply.github.com>
2024-05-04 19:20:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -261,7 +261,7 @@ if selected == "HOMEPAGE":
     st.pyplot(fig)
 
 if selected == "STOCK":
-    tab1, tab8 = st.tabs(["GENERAL INFO", "FUNDAMENTALS"])
+    tab1= st.tabs(["GENERAL INFO"])
     with tab1:
         df = pd.read_csv('STOCKDATA.csv')
         dataa = st.selectbox('Which stock do you want to analyze?', df['Security Id'])
